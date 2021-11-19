d183e7f3cb75358aa274149bd79dd093ca121fc9
antlerhead <antlerhead@hotmail.co.uk>
2021-11-19 11:04:02 +0000

    first commit

---
+++ app/src/test/java/TakeAwayChallengeJava/TakeAwayTest.java
@@ -0,0 +1,18 @@
+package TakeAwayChallengeJava;
+
+public class TakeAwayTest {
+    
+        // @Test public void testGetsWordToGuess() {
+        //     WordChoser mockedChoser = mock(WordChoser.class);
+        //     Game game = new Game(mockedChoser);
+        //     assertTrue(game.getWordToGuess() instanceof String);
+        // }
+
+
+        // As a customer
+        // So that I can check if I want to order something
+        // I would like to see a list of dishes with prices
+
+        @Test public void 
+
+}
